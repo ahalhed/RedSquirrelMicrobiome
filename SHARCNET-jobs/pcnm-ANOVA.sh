7cc7e0ef952c70c3052b349130db7997000c435e
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2020-04-07 07:38:11 -0400

    PCNM modifications

---
+++ SHARCNET-jobs/pcnm-ANOVA.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=def-cottenie
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu 16G
+#SBATCH --job-name=pcnm-ANOVA
+#SBATCH --output=../output/%x-%j.out
+
+#script starts here
+#----------------------------------
+
+#set up
+cd /home/ahalhed/red-squirrel-w2020/R-env/
+
+module load nixpkgs/16.09
+module load gcc/7.3.0
+module load r/3.6.0
+R
+
+# run R script
+Rscript /home/ahalhed/red-squirrel-w2020/R-env/pcnm-ANOVA.R
