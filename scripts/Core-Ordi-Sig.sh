66ebbcb906878adcedacb51454ca43ee653c35bc
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2020-05-04 07:17:34 -0400

    Update Core-Ordi-Sig.sh

--- scripts/Core-Ordi-Sig.sh
+++ scripts/Core-Ordi-Sig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=def-cottenie
-#SBATCH --time=0-01:00:00
+#SBATCH --time=0-00:15:00
 #SBATCH --mem-per-cpu 4G
 #SBATCH --job-name=Core-Ordi-Sig
 #SBATCH --output=/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/%x-%j.out
@@ -10,8 +10,6 @@
 #set up
 # initiated frpm: /home/ahalhed/red-squirrel-w2020/R-env/
 module load nixpkgs/16.09 gcc/7.3.0 r/3.6.0
-R
 
 # run R script
-# replace KL08 with specific grid/year combo being run
 Rscript /home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/scripts/significant-ordisurf-core.R
