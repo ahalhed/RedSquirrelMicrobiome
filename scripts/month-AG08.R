a4e2f7b543111c845130107cfebcbb37f67e16ed
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2020-05-22 11:48:10 -0400

    New monthly scripts

---
+++ scripts/month-AG08.R
@@ -0,0 +1,473 @@
+#---
+#title: "PCNM for Core Squirrel Microbiome by Month(SHARCNET)"
+#author: "Alicia Halhed"
+#date: "05/22/2020"
+#---
+
+print("Set up (working directory, theme, and packages)")
+# set working directory
+#setwd("/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial")
+
+# attach required packages
+library(tidyverse)
+library(qiime2R)
+library(phyloseq)
+library(vegan)
+
+# set theme for plots
+theme_set(theme_bw())
+
+print("Initiate functions for analysis")
+# subset the metadata by grid and year
+met_year <- function(metadata, grid, year) {
+  met <- rownames_to_column(metadata, var = "SampleID")
+  df1 <- subset(met, Grid == grid)
+  df2 <- subset(df1, Year == year)
+  df3 <- column_to_rownames(remove_rownames(df2), var = "SampleID")
+  return(df3)
+}
+# subset the XY's by grid, year, and month
+XY_month <- function(metadata, grid, year, month) {
+  met <- rownames_to_column(metadata, var = "SampleID")
+  df1 <- subset(met, Grid == grid, 
+                select = c("SampleID", "Location.X", "Location.Y", "Year", "Month"))
+  df2 <- subset(df1, Year == year, 
+                select = c("SampleID", "Location.X", "Location.Y", "Month"))
+  df3 <- subset(df2, Month == month, 
+                select = c("SampleID", "Location.X", "Location.Y"))
+  df4 <- column_to_rownames(remove_rownames(df3), var = "SampleID")
+  return(df4)
+}
+# maximum distance
+max_dist <- function(dm) {
+  df1 <- as.data.frame(as.matrix(dm))
+  # functions is soft deprecated (replace with functions or lambdas)
+  summ <- summarise_each(df1, ~ max(df1, na.rm=TRUE))
+  m <- apply(summ, 1, max)
+  return(m)
+}
+# community object
+comm_obj <- function(XY, c) {
+  # subset the OTUs (c is OTU table being subset)
+  comm <- c %>%
+    subset(., rownames(.) %in% rownames(XY)) %>%
+    .[ , colSums(.)>0 ]
+  return(comm)
+}
+# subset full metadata by month
+met_month <- function(XY, meta) {
+  # subset for samples in the XY
+  df1 <- subset(meta, rownames(meta) %in% rownames(XY))
+  # remove NAs
+  df2 <- df1 %>% select_if(~ !any(is.na(.)))
+  # remove location information
+  df3 <- select(df2, -c('Location.X', 'Location.Y', Location))
+  # select columns with more than one level
+  df4 <- df3[sapply(df3, function(x) length(unique(x))>1)]
+  return(df4)
+}
+
+# get the data
+print("Read in the Data")
+print("Building phyloseq object")
+ps <- qza_to_phyloseq(features = "/home/ahalhed/red-squirrel-w2020/filtered-table.qza",
+                      tree = "/home/ahalhed/red-squirrel-w2020/trees/rooted_tree.qza",
+                      taxonomy = "/home/ahalhed/red-squirrel-w2020/taxonomy/GG-taxonomy.qza",
+                      metadata = "/home/ahalhed/red-squirrel-w2020/input/RS_meta.tsv")
+# based on the meta function from the microbiome package
+# I don't want to load a whole package for one function
+print("Read in the metadata")
+rs_q2_metadata <- as(sample_data(ps), "data.frame")
+rownames(rs_q2_metadata) <- sample_names(ps)
+
+## Core and rare divide
+print("Finding core microbiome")
+print("Accessing full OTU table as a community object")
+OTU_full <- otu_table(ps) %>% as.matrix %>% 
+  as.data.frame %>% 
+  t %>% as.data.frame 
+# transform the counts to relative abundance
+print("Transforming the OTU counts to relative abundance")
+RA_full <- transform_sample_counts(ps, function(x) x/sum(x)) %>% 
+  otu_table %>% as.matrix %>% as.data.frame %>% 
+  t %>% as.data.frame
+# 95% threshold
+# minimum number of samples that an OTU must occur in 
+# to be considered as part of the "core" microbiome
+print("Calculating 95% threshold")
+th <- 0.95*nrow(RA_full)
+th
+# Access the number of non-zero occurrences of each OTU
+print("Accessing the number of non-zero occurrences of each OTU")
+nonzero <- function(x) sum(x!=0)
+nz <- plyr::numcolwise(nonzero)(RA_full)
+# average relative abundance across all 909 samples
+print("Calculating average relative abundance")
+RA <- RA_full %>% colSums() %>% as.data.frame
+RA_001 <- RA %>%
+  rownames_to_column(var = "OTU") %>% 
+  rename("ColSum" = ".") %>% 
+  mutate(RelativeAbundance = .$ColSum/nrow(OTU_full)) %>%
+  # greater than 0.001 (or 0.1%)
+  filter(RelativeAbundance > 0.001)
+# extract the OTUs that fall above the 95% threshold
+# based on the number of non-zero occurrences of each OTU
+print("Extract the OTUs that occur in >95% of samples")
+OTU95 <- t(nz) %>% as.data.frame %>% 
+  rownames_to_column(var = "OTU") %>% 
+  rename("Occurrences" = "V1") %>%
+  subset(., Occurrences > th)
+# combine to ensure that those in 95% of the samples
+# have a total relative abundance of 0.1%
+print("Find OTUs that occur in >95% of samples and have an average relative abundance >0.1%")
+cOTU <- inner_join(OTU95, RA_001)
+print("How many OTUs meet these criteria?")
+nrow(cOTU)
+# make the new data frames
+print("Subset the OTU table to find core and rare OTUs")
+OTU_core <- OTU_full[, cOTU$OTU]
+OTU_rare <- OTU_full %>% 
+  select(-one_of(cOTU$OTU))
+
+## XY data
+print("Accessing the XY data by month")
+# start by getting the metadata for the grid/year
+met <- met_year(rs_q2_metadata, "AG", 2008)
+# loop to create individual month data frames
+for (month in unique(met$Month)) {
+  df <- XY_month(rs_q2_metadata, "AG", 2008, month)
+  assign(paste('Month',month,sep = ' '),df)
+  rm(df, month)
+}
+# make a list of the data frames generated from the loop
+XY_list <- do.call("list",
+                   # searching the global environment for the pattern
+                   mget(grep("Month", names(.GlobalEnv), value=TRUE)))
+print("Months in this Grid")
+unique(met$Month)
+print("Computing Euclidean Distances")
+dist_list <- lapply(XY_list, dist)
+print("Maximum Euclidean Distance by Month")
+lapply(dist_list, max_dist)
+## community objects
+# subset the samples from the core microbiome
+print("Build the community object (OTU table) for grid/year/month")
+commFull <- lapply(XY_list, comm_obj, c=OTU_full)
+commCore <- lapply(XY_list, comm_obj, c=OTU_core)
+commRare <- lapply(XY_list, comm_obj, c=OTU_rare)
+## metadata
+# sample ID's are rownames
+# get the metadata subset
+print("Extract metadata for grid/year/month")
+# extracting only non-location metadata columns
+# with more than one unique level
+met_list <- lapply(XY_list, met_month, meta=rs_q2_metadata)
+
+## Clean up 1
+# Remove objects we're done with
+print("Removing pobjects that are no longer needed")
+rm(rs_q2_metadata, cOTU, OTU95, RA_001, RA, RA_full, OTU_full, ps, OTU_core, OTU_rare, nz)
+# not removing the Months because they're small and not the same across all
+
+
+
+## Analysis time!
+# unweighted PCNM
+print("Unweighted PCNM - for use with all OTU tables")
+pcnm_list <- lapply(dist_list, pcnm)
+# need to sort out how to generalize printing  month # with the vectors
+for (month in pcnm_list) {
+  print(month$vectors)
+}
+print("Acessing PCNM scores")
+scores_list <- lapply(pcnm_list, scores)
+
+
+# core OTUs
+print("Analysis for Core OTUs")
+print("Variance partitioning - Core OTUs")
+vp_mod1_list <- mapply(varpart, commCore, scores_list, data=met_list, 
+                       MoreArgs = list(~., transfo = "hel"),
+                       SIMPLIFY = FALSE)
+vp_mod1_list
+# plot the partitioning
+pdf(file = "/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/plots/core_AG2008_vp_mod1M.pdf")
+# make plot
+# plotted in numerical order by month
+lapply(vp_mod1_list, plot)
+dev.off()
+#remove vp object, to repeat with new OTU table
+rm(vp_mod1_list)
+
+# test with RDA
+print("Testing with RDA (full model) - core OTUS")
+deco_list <- lapply(commCore, decostand, "hel")
+# create a tiny anonymous function to include formula syntax in call
+abFrac <- mapply(function(x,data) rda(x~., data), 
+                 deco_list, met_list, SIMPLIFY=FALSE)
+abFrac # Full model
+lapply(abFrac, anova, step=200, perm.max=1000)
+# RsquareAdj gives the same result as component [a] of varpart
+lapply(abFrac, RsquareAdj)
+
+# Test fraction [a] using partial RDA:
+print("Testing with partial RDA (fraction [a]) - core OTUS")
+# create a tiny anonymous function to include formula syntax in call
+aFrac <- mapply(function(x,y,data) rda(x~.+Condition(scores(y)), data), 
+                deco_list, pcnm_list, met_list, SIMPLIFY=FALSE)
+aFrac
+lapply(aFrac, anova, step=200, perm.max=1000)
+# RsquareAdj gives the same result as component [a] of varpart
+lapply(aFrac, RsquareAdj)
+
+# forward selection for parsimonious model
+print("Forward selection for parsimonious model - core OTUs")
+# env variables
+print("Environmental variables - core OTUs")
+# create a tiny anonymous function to include formula syntax in call
+abFrac0 <- mapply(function(x,data) rda(x~1, data), 
+                  deco_list, met_list, SIMPLIFY=FALSE) # Reduced model
+step.env <- mapply(function(x,y) ordiR2step(x, scope = formula(y)), 
+                   abFrac0, abFrac, SIMPLIFY=FALSE)
+step.env # an rda model, with the final model predictor variables
+
+print("Summary of environmental selection process - core OTUs")
+lapply(step.env, function(x) x$anova)
+print("ANOVA on full environmental selection - core OTUs")
+lapply(step.env, anova)
+
+# save plot
+pdf(file = "/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/plots/core_AG2008_step_envM.pdf")
+# make plot
+lapply(step.env, plot)
+dev.off()
+
+# spatial variables
+print("Spatial variables - core OTU")
+pcnm_df <- lapply(pcnm_list, function(x) as.data.frame(scores(x)))
+bcFrac <- mapply(function(x,data) rda(x~., data), 
+                 deco_list, pcnm_df, SIMPLIFY=FALSE) # Full model
+bcFrac0 <- mapply(function(x,data) rda(x~1, data), 
+                  deco_list, pcnm_df, SIMPLIFY=FALSE) # Reduced model
+step.space <- mapply(function(x,y) ordiR2step(x, scope = formula(y)), 
+                     bcFrac0, bcFrac, SIMPLIFY=FALSE)
+step.space
+
+print("Summary of spatial selection process - core OTU")
+lapply(step.space, function(x) x$anova)
+print("ANOVA on full spatial selection - core OTU")
+lapply(step.space, anova)
+
+# save plot
+pdf(file = "/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/plots/core_AG2008_step_spaceM.pdf")
+# make plot
+lapply(step.space, plot)
+dev.off()
+
+print("Partition Bray-Curtis dissimilarities - core OTUs")
+vdist <- lapply(commCore, vegdist)
+pbcd <- mapply(function(x,y,z) varpart(x, ~., y, data = z),
+               vdist, scores_list, met_list, SIMPLIFY=FALSE)
+pbcd
+
+#cleanup
+# remove objects to be replaced
+rm(vdist,pbcd)
+rm(deco_list, abFrac, aFrac,abFrac0, step.env, pcnm_df, bcFrac, bcFrac0, step.space)
+
+
+# Rare OTUs
+print("Analysis for Rare OTUs")
+print("Variance partitioning - Rare OTUs")
+vp_mod1_list <- mapply(varpart, commRare, scores_list, data=met_list, 
+                       MoreArgs = list(~., transfo = "hel"),
+                       SIMPLIFY = FALSE)
+vp_mod1_list
+# plot the partitioning
+pdf(file = "/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/plots/rare_AG2008_vp_mod1M.pdf")
+# make plot
+# plotted in numerical order by month
+lapply(vp_mod1_list, plot)
+dev.off()
+#remove vp object, to repeat with new OTU table
+rm(vp_mod1_list)
+
+# test with RDA
+print("Testing with RDA (full model) - rare OTUS")
+deco_list <- lapply(commRare, decostand, "hel")
+# create a tiny anonymous function to include formula syntax in call
+abFrac <- mapply(function(x,data) rda(x~., data), 
+                 deco_list, met_list, SIMPLIFY=FALSE)
+abFrac # Full model
+# anova
+lapply(abFrac, anova, step=200, perm.max=1000)
+# RsquareAdj gives the same result as component [a] of varpart
+lapply(abFrac, RsquareAdj)
+
+# Test fraction [a] using partial RDA:
+print("Testing with partial RDA (fraction [a]) - rare OTUS")
+# create a tiny anonymous function to include formula syntax in call
+aFrac <- mapply(function(x,y,data) rda(x~.+Condition(scores(y)), data), 
+                deco_list, pcnm_list, met_list, SIMPLIFY=FALSE)
+aFrac
+# anova
+lapply(aFrac, anova, step=200, perm.max=1000)
+# RsquareAdj gives the same result as component [a] of varpart
+lapply(aFrac, RsquareAdj)
+
+# forward selection for parsimonious model
+print("Forward selection for parsimonious model - rare OTUs")
+# env variables
+print("Environmental variables - rare OTUs")
+# create a tiny anonymous function to include formula syntax in call
+abFrac0 <- mapply(function(x,data) rda(x~1, data), 
+                  deco_list, met_list, SIMPLIFY=FALSE) # Reduced model
+
+step.env <- mapply(function(x,y) ordiR2step(x, scope = formula(y)), 
+                   abFrac0, abFrac, SIMPLIFY=FALSE)
+step.env # an rda model, with the final model predictor variables
+
+print("Summary of environmental selection process - rare OTUs")
+lapply(step.env, function(x) x$anova)
+print("ANOVA on full environmental selection - rare OTUs")
+lapply(step.env, anova)
+
+# save plot
+pdf(file = "/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/plots/rare_AG2008_step_envM.pdf")
+# make plot
+lapply(step.env, plot)
+dev.off()
+
+# spatial variables
+print("Spatial variables - rare OTU")
+pcnm_df <- lapply(pcnm_list, function(x) as.data.frame(scores(x)))
+bcFrac <- mapply(function(x,data) rda(x~., data), 
+                 deco_list, pcnm_df, SIMPLIFY=FALSE) # Full model
+bcFrac0 <- mapply(function(x,data) rda(x~1, data), 
+                  deco_list, pcnm_df, SIMPLIFY=FALSE) # Reduced model
+step.space <- mapply(function(x,y) ordiR2step(x, scope = formula(y)), 
+                     bcFrac0, bcFrac, SIMPLIFY=FALSE)
+step.space
+
+# summary of selection process
+print("Summary of spatial selection process - rare OTU")
+lapply(step.space, function(x) x$anova)
+print("ANOVA on full spatial selection - rare OTU")
+lapply(step.space, anova)
+
+# save plot
+pdf(file = "/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/plots/rare_AG2008_step_spaceM.pdf")
+# make plot
+lapply(step.space, plot)
+dev.off()
+
+print("Partition Bray-Curtis dissimilarities - rare OTUs")
+vdist <- lapply(commRare, vegdist)
+pbcd <- mapply(function(x,y,z) varpart(x, ~., y, data = z),
+               vdist, scores_list, met_list, SIMPLIFY=FALSE)
+pbcd
+
+#cleanup
+# remove objects to be replaced
+rm(vdist,pbcd)
+rm(deco_list, abFrac, aFrac,abFrac0, step.env, pcnm_df, bcFrac, bcFrac0, step.space)
+
+print("Variance partitioning - All OTUs")
+vp_mod1_list <- mapply(varpart, commCore, scores_list, data=met_list, 
+                       MoreArgs = list(~., transfo = "hel"),
+                       SIMPLIFY = FALSE)
+vp_mod1_list
+# plot the partitioning
+pdf(file = "/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/plots/AG2008_vp_mod1M.pdf")
+# make plot
+# plotted in numerical order by month
+lapply(vp_mod1_list, plot)
+dev.off()
+#remove vp object, done with it now
+rm(vp_mod1_list)
+
+# test with RDA
+print("Testing with RDA (full model) - all OTUS")
+deco_list <- lapply(commFull, decostand, "hel")
+# create a tiny anonymous function to include formula syntax in call
+abFrac <- mapply(function(x,data) rda(x~., data), 
+                 deco_list, met_list, SIMPLIFY=FALSE)
+
+abFrac # Full model
+
+lapply(abFrac, anova, step=200, perm.max=1000)
+# RsquareAdj gives the same result as component [a] of varpart
+lapply(abFrac, RsquareAdj)
+
+# Test fraction [a] using partial RDA:
+print("Testing with partial RDA (fraction [a]) - all OTUS")
+# create a tiny anonymous function to include formula syntax in call
+aFrac <- mapply(function(x,y,data) rda(x~.+Condition(scores(y)), data), 
+                deco_list, pcnm_list, met_list, SIMPLIFY=FALSE)
+aFrac
+# anova
+lapply(aFrac, anova, step=200, perm.max=1000)
+# RsquareAdj gives the same result as component [a] of varpart
+lapply(aFrac, RsquareAdj)
+
+# forward selection for parsimonious model
+print("Forward selection for parsimonious model - all OTUs")
+
+# env variables
+print("Environmental variables - all OTUs")
+# create a tiny anonymous function to include formula syntax in call
+abFrac0 <- mapply(function(x,data) rda(x~1, data), 
+                  deco_list, met_list, SIMPLIFY=FALSE) # Reduced model
+
+step.env <- mapply(function(x,y) ordiR2step(x, scope = formula(y)), 
+                   abFrac0, abFrac, SIMPLIFY=FALSE)
+
+step.env # an rda model, with the final model predictor variables
+
+print("Summary of environmental selection process - all OTUs")
+lapply(step.env, function(x) x$anova)
+print("ANOVA on full environmental selection - all OTUs")
+lapply(step.env, anova)
+
+# save plot
+pdf(file = "/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/plots/AG2008_step_envM.pdf")
+# make plot
+lapply(step.env, plot)
+dev.off()
+
+# spatial variables
+print("Spatial variables - all OTU")
+pcnm_df <- lapply(pcnm_list, function(x) as.data.frame(scores(x)))
+bcFrac <- mapply(function(x,data) rda(x~., data), 
+                 deco_list, pcnm_df, SIMPLIFY=FALSE) # Full model
+bcFrac0 <- mapply(function(x,data) rda(x~1, data), 
+                  deco_list, pcnm_df, SIMPLIFY=FALSE) # Reduced model
+step.space <- mapply(function(x,y) ordiR2step(x, scope = formula(y)), 
+                     bcFrac0, bcFrac, SIMPLIFY=FALSE)
+step.space
+
+# summary of selection process
+print("Summary of spatial selection process - all OTU")
+lapply(step.space, function(x) x$anova)
+print("ANOVA on full spatial selection - all OTU")
+lapply(step.space, anova)
+
+# save plot
+pdf(file = "/home/ahalhed/red-squirrel-w2020/R-env/RedSquirrelSpatial/plots/AG2008_step_spaceM.pdf")
+# make plot
+lapply(step.space, plot)
+dev.off()
+
+print("Partition Bray-Curtis dissimilarities - all OTUs")
+vdist <- lapply(commFull, vegdist)
+pbcd <- mapply(function(x,y,z) varpart(x, ~., y, data = z),
+               vdist, scores_list, met_list, SIMPLIFY=FALSE)
+pbcd
+
+#cleanup
+# remove objects to be replaced
+rm(vdist,pbcd)
+rm(deco_list, abFrac, aFrac,abFrac0, step.env, pcnm_df, bcFrac, bcFrac0, step.space)
+
+# I have removed the variation decomposition with parsimonious variables, 
+# since it was frequently failing and would likely cause issues.
