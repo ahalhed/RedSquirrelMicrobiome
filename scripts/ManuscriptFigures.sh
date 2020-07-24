f8e6a8fef2110c69c212b05108efc5cae050b38b
Alicia Halhed <ahalhed@uoguelph.ca>
2020-07-24 15:35:15 -0400

    Plot updates

--- scripts/ManuscriptFigures.sh
+++ scripts/ManuscriptFigures.sh
@@ -16,3 +16,4 @@
 module load nixpkgs/16.09 gcc/7.3.0 r/3.6.0
 # run R script
 Rscript /home/ahalhed/projects/def-cottenie/Microbiome/RedSquirrelMicrobiome/R-env/RedSquirrelSpatial/scripts/ManuscriptFigures.R
+# resulting tsv files get gzipped after running the script, as they are large and won't commit to github
