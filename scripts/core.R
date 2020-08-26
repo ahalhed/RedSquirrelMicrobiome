380ac28ddf685c64d6ea792ae522a371cfd756eb
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2020-08-26 09:40:15 -0400

    Update core.R
    Removing the aitchison thing

--- scripts/core.R
+++ scripts/core.R
@@ -3,12 +3,11 @@ setwd("/home/ahalhed/projects/def-cottenie/Microbiome/RedSquirrelMicrobiome/R-en
 #devtools::install_github("jbisanz/qiime2R")
 library(qiime2R)
 library(vegan)
-library(zCompositions)
-# devtools::install_github('ggloor/CoDaSeq/CoDaSeq')
-library(CoDaSeq)
 library(tidyverse)
 
 theme_set(theme_bw())
+
+nReads <- 4000  #data needs to rarefied
 
 otu <- read_qza("/home/ahalhed/projects/def-cottenie/Microbiome/RedSquirrelMicrobiome/filtered-table-10.qza")$data
 map <- read_q2metadata("/home/ahalhed/projects/def-cottenie/Microbiome/RedSquirrelMicrobiome/input/RS_meta.tsv") # this is metadata
@@ -18,18 +17,13 @@ otu_PA <- 1*((otu>0)==1)                                               # presenc
 otu_occ <- rowSums(otu_PA)/ncol(otu_PA)                                # occupancy calculation
 otu_rel <- apply(decostand(otu, method="total", MARGIN=2),1, mean)     # mean relative abundance
 occ_abun <- rownames_to_column(as.data.frame(cbind(otu_occ, otu_rel)),'otu') # combining occupancy and abundance data frame
-# using a compositional approach
-# impute the OTU table (takes OTUs as columns)
-OTUimp <- cmultRepl(t(otu), label=0, method="CZM") # all OTUs
-# compute the aitchison values (subsequent analyses needs OTUs as columns)
-OTUclr <- t(codaSeq.clr(OTUimp))
 
 # Ranking OTUs based on their occupancy
 # For caluclating raking index we included following conditions:
 #   - time-specific occupancy (sumF) = frequency of detection within time point (genotype or site)
 #   - replication consistency (sumG) = has occupancy of 1 in at least one time point (genotype or site) (1 if occupancy 1, else 0)
 
-PresenceSum <- data.frame(otu = as.factor(row.names(otu)), OTUclr) %>% 
+PresenceSum <- data.frame(otu = as.factor(row.names(otu)), otu) %>% 
   gather(SampleID, abun, -otu) %>%
   left_join(map, by = 'SampleID') %>%
   group_by(otu, CollectionDate) %>%
@@ -47,72 +41,72 @@ otu_ranked <- occ_abun %>%
             rank=Index) %>%
   arrange(desc(rank))
 
-# Calculating the contribution of ranked OTUs to the euclidean similarity
-EUCaddition <- NULL
+# Calculating the contribution of ranked OTUs to the Bray-Curtis similarity
+BCaddition <- NULL
 
-# calculating EUC dissimilarity based on the 1st ranked OTU
+# calculating BC dissimilarity based on the 1st ranked OTU
 otu_start=otu_ranked$otu[1]                   
 start_matrix <- as.matrix(otu[otu_start,])
 start_matrix <- t(start_matrix)
-x <- apply(combn(ncol(start_matrix), 2), 2, function(x) dist(start_matrix[,x[1:2]]))
+x <- x <- apply(combn(ncol(start_matrix), 2), 2, function(x) sum(abs(start_matrix[,x[1]]- start_matrix[,x[2]]))/(2*nReads))
 x_names <- apply(combn(ncol(start_matrix), 2), 2, function(x) paste(colnames(start_matrix)[x], collapse=' - '))
 df_s <- data.frame(x_names,x)
 names(df_s)[2] <- 1 
-EUCaddition <- rbind(EUCaddition,df_s)
-# calculating EUC dissimilarity based on additon of ranked OTUs from 2nd to 500th. Can be set to the entire length of OTUs in the dataset, however it might take some time if more than 5000 OTUs are included.
+BCaddition <- rbind(BCaddition,df_s)
+# calculating BC dissimilarity based on additon of ranked OTUs from 2nd to 500th. Can be set to the entire length of OTUs in the dataset, however it might take some time if more than 5000 OTUs are included.
 for(i in 2:500){                              
   otu_add=otu_ranked$otu[i]                       
   add_matrix <- as.matrix(otu[otu_add,])
   add_matrix <- t(add_matrix)
   start_matrix <- rbind(start_matrix, add_matrix)
-  x <- apply(combn(ncol(start_matrix), 2), 2, function(x) dist(start_matrix[,x[1:2]]))
+  x <- apply(combn(ncol(start_matrix), 2), 2, function(x) sum(abs(start_matrix[,x[1]]- start_matrix[,x[2]]))/(2*nReads))
   x_names <- apply(combn(ncol(start_matrix), 2), 2, function(x) paste(colnames(start_matrix)[x], collapse=' - '))
   df_a <- data.frame(x_names,x)
   names(df_a)[2] <- i 
-  EUCaddition <- left_join(EUCaddition, df_a, by=c('x_names'))
+  BCaddition <- left_join(BCaddition, df_a, by=c('x_names'))
 }
-# calculating the EUC dissimilarity of the whole dataset (not needed if the second loop is already including all OTUs) 
-x <-  apply(combn(ncol(otu), 2), 2, function(x) dist(start_matrix[,x[1:2]]))   
+# calculating the BC dissimilarity of the whole dataset (not needed if the second loop is already including all OTUs) 
+x <-  apply(combn(ncol(otu), 2), 2, function(x) sum(abs(start_matrix[,x[1]]- start_matrix[,x[2]]))/(2*nReads))   
 x_names <- apply(combn(ncol(otu), 2), 2, function(x) paste(colnames(otu)[x], collapse=' - '))
 df_full <- data.frame(x_names,x)
 names(df_full)[2] <- length(rownames(otu))
-EUCfull <- left_join(EUCaddition,df_full, by='x_names')
+BCfull <- left_join(BCaddition,df_full, by='x_names')
 
-rownames(EUCfull) <- EUCfull$x_names
-temp_EUC <- EUCfull
-temp_EUC$x_names <- NULL
-temp_EUC_matrix <- as.matrix(temp_EUC)
+rownames(BCfull) <- BCfull$x_names
+temp_BC <- BCfull
+temp_BC$x_names <- NULL
+temp_BC_matrix <- as.matrix(temp_BC)
 
-EUC_ranked <- data.frame(rank = as.factor(row.names(t(temp_EUC_matrix))),t(temp_EUC_matrix)) %>% 
-  gather(comparison, EUC, -rank) %>%
+BC_ranked <- data.frame(rank = as.factor(row.names(t(temp_BC_matrix))),t(temp_BC_matrix)) %>% 
+  gather(comparison, BC, -rank) %>%
   group_by(rank) %>%
-  summarise(MeanEUC=mean(EUC)) %>%            # mean Bray-Curtis dissimilarity
-  arrange(desc(-MeanEUC)) %>%
-  mutate(proportionEUC=MeanEUC/max(MeanEUC))   # proportion of the dissimilarity explained by the n number of ranked OTUs
-Increase=EUC_ranked$MeanEUC[-1]/EUC_ranked$MeanEUC[-length(EUC_ranked$MeanEUC)]
-increaseDF <- data.frame(IncreaseEUC=c(0,(Increase)), rank=factor(c(1:(length(Increase)+1))))
-EUC_ranked <- left_join(EUC_ranked, increaseDF)
-EUC_ranked <- EUC_ranked[-nrow(EUC_ranked),]
+  summarise(MeanBC=mean(BC)) %>%            # mean Bray-Curtis dissimilarity
+  arrange(desc(-MeanBC)) %>%
+  mutate(proportionBC=MeanBC/max(MeanBC))   # proportion of the dissimilarity explained by the n number of ranked OTUs
+Increase=BC_ranked$MeanBC[-1]/BC_ranked$MeanBC[-length(BC_ranked$MeanBC)]
+increaseDF <- data.frame(IncreaseBC=c(0,(Increase)), rank=factor(c(1:(length(Increase)+1))))
+BC_ranked <- left_join(BC_ranked, increaseDF)
+BC_ranked <- BC_ranked[-nrow(BC_ranked),]
 
 #Creating thresholds for core inclusion 
 
 #Method: 
 #A) Elbow method (first order difference) (script modified from https://pommevilla.github.io/random/elbows.html)
 fo_difference <- function(pos){
-  left <- (EUC_ranked[pos, 2] - EUC_ranked[1, 2]) / pos
-  right <- (EUC_ranked[nrow(EUC_ranked), 2] - EUC_ranked[pos, 2]) / (nrow(EUC_ranked) - pos)
+  left <- (BC_ranked[pos, 2] - BC_ranked[1, 2]) / pos
+  right <- (BC_ranked[nrow(BC_ranked), 2] - BC_ranked[pos, 2]) / (nrow(BC_ranked) - pos)
   return(left - right)
 }
-EUC_ranked$fo_diffs <- sapply(1:nrow(EUC_ranked), fo_difference)
+BC_ranked$fo_diffs <- sapply(1:nrow(BC_ranked), fo_difference)
 
-elbow <- which.max(EUC_ranked$fo_diffs)
+elbow <- which.max(BC_ranked$fo_diffs)
 
-#B) Final increase in EUC similarity of equal or greater then 2% 
-lastCall <- last(as.numeric(EUC_ranked$rank[(EUC_ranked$IncreaseEUC>=1.02)]))
+#B) Final increase in BC similarity of equal or greater then 2% 
+lastCall <- last(as.numeric(BC_ranked$rank[(BC_ranked$IncreaseBC>=1.02)]))
 
 #Creating occupancy abundance plot
 occ_abun$fill <- 'no'
-occ_abun$fill[occ_abun$otu %in% otu_ranked$otu[1:last(as.numeric(EUC_ranked$rank[(EUC_ranked$IncreaseEUC>=1.02)]))]] <- 'core'
+occ_abun$fill[occ_abun$otu %in% otu_ranked$otu[1:last(as.numeric(BC_ranked$rank[(BC_ranked$IncreaseBC>=1.02)]))]] <- 'core'
 # add 95% occupancy threshold for core
 occ_abun$Community <- ifelse(occ_abun$otu_occ >= 0.95 & occ_abun$fill == "core", "Confirmed Core",
                              ifelse(occ_abun$otu_occ < 0.95 & occ_abun$fill == "core", "Core Candidate",
